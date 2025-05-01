3c105cbcad75e282e11ebf49b540137f8273d5d4
songgaeun <rkdmsdmqek1128@gmail.com>
2025-05-01 15:40:39 +0900

    init

---
+++ src/main/java/com/example/schedulesproject/SchedulesProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.schedulesproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulesProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulesProjectApplication.class, args);
+    }
+
+}
